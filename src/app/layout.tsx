a71bd61df3494318d307ca679afbbd1544ebc8bc
Lance Chiu <33804107+xmdbro@users.noreply.github.com>
2026-09-22 18:29:47 +0800

    feat: noindex, nofollow, nocache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,16 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Lance Chiu",
   description: "hi@lancechiu.com",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
